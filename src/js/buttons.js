ba5396fcf26f80fe3c3632b947d24e620a1cad72
Patrick Neal <patrickn@imagemattersllc.com>
2015-10-27 13:42:55 -0400

    fix js hook enabling btn-group-multi trigger

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -3,9 +3,12 @@
    Click support for "multi" buttons
 \* ================================== */
 
-$('.btn-group-multi > .btn-multi').on('click', function(evt) {
+$(document)
+    .on('click', '.btn-group-multi > .btn-multi', function (e) {
+
     var tgt = $(evt.target);
     if(!tgt.hasClass('btn-group-multi'))
         tgt = tgt.closest('.btn-group-multi');
     tgt.toggleClass('active');
+    
 });

cb2239134e8152c5e839c26243b3f7f29bb415e8
Patrick Neal <patrickn@imagemattersllc.com>
2016-07-06 14:58:44 -0400

    added style for input-group-addon for material forms, js hooks for input-group-slick inside material forms

--- src/js/material.js
+++ src/js/material.js
@@ -14,7 +14,10 @@
 
     var GROUP_SELECTOR = '.form-group.t-material';
 
-    var selector = GROUP_SELECTOR + ' > .form-control, ' + GROUP_SELECTOR + ' > .input-group > .form-control';
+    var selector = GROUP_SELECTOR + ' > .form-control, ' + 
+                   GROUP_SELECTOR + ' > .input-group > .form-control, ' + 
+                   GROUP_SELECTOR + ' > .input-group-slick > .form-control';
+                   
     var switchSelector = GROUP_SELECTOR + '.switch .lever';
     var requiredSelector = GROUP_SELECTOR + ' .form-control[required]';
 

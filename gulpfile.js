4d759e3f06db48ba80ec367f0a1dc7f02d0ab1f2
Patrick Neal <patrickn@imagemattersllc.com>
2019-06-21 15:39:25 -0400

    upgrade to gulp 4, update documentation, remove outdated examples, remove npm warnings

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 
-var pkg         = require('./package.json'),
-    gulp        = require('gulp'),
+const   { src, dest, pipe, watch, task, series } = require('gulp');
+
+const pkg         = require('./package.json'),
     jshint      = require('gulp-jshint'),
     concat      = require('gulp-concat'),
     babel       = require('gulp-babel'),
@@ -27,75 +28,71 @@ const autoprefix = new autoprefixer({
 
 
 
-require('gulp-help')(gulp, { description: 'Help listing.' });
-
-
-gulp.task('jshint', function () {
-    gulp.src(['src/**/*.js'])
-        .pipe(jshint({
-            // laxbreak: true,
-            // laxcomma: true,
-            esversion: 6, //JSHint Harmony/ES6
-            // eqnull: true,
-            browser: true,
-            jquery: true
-        }))
-        .pipe(jshint.reporter('default'))
-        .pipe(livereload());
-});
+function jshintTask () {
+    return src( ['src/**/*.js'] )
+    .pipe( jshint({
+        // laxbreak: true,
+        // laxcomma: true,
+        esversion: 6, //JSHint Harmony/ES6
+        // eqnull: true,
+        browser: true,
+        jquery: true
+    }) )
+    .pipe( jshint.reporter('default') )
+    .pipe( livereload() );
+}
 
 
 
-gulp.task('js', 'Concat, Uglify JavaScript into a single file', function() {
+// Concat, Uglify JavaScript into a single file
+function jsTask() {
 
     //main application
-    gulp.src([ 'src/js/**/*.js' ])
-        // .pipe(srcmaps.init())
-        .pipe(concat('geoplatform.style.js'))
-        .pipe(babel({presets: ["es2015"]}))
-        .pipe(uglify()).on('error', notify.onError("Error: <%= error.message %>"))
-        .pipe(rename({extname: ".min.js"}))
-        // .pipe(srcmaps.write('./'))
-        .pipe(gulp.dest('dist/js/'))
-        .pipe(notify('Compiled JS'));
-});
+    return src( [ 'src/js/**/*.js' ] )
+    // .pipe(srcmaps.init())
+    .pipe( concat('geoplatform.style.js') )
+    .pipe( babel({presets: ["env"]}) )
+    .pipe( uglify()).on( 'error', notify.onError("Error: <%= error.message %>") )
+    .pipe( rename({extname: ".min.js"}) )
+    // .pipe(srcmaps.write('./') )
+    .pipe( dest('dist/js/') )
+    .pipe( notify('Compiled JS') );
+}
 
-gulp.task('less', 'Compile less into a single file.', function() {
+//Compile less into a single file.
+function lessTask() {
 
     //Build GP platform style include
-    gulp.src([
-            'src/less/style.less'
-        ], {base: "."})
-        .pipe(concat('platform.less'))
-        //must write to public before processing for paths to work
-        // .pipe(gulp.dest('dist/css/'))
-        .pipe(less({
-            plugins: [autoprefix],
-            paths: ['./src/less']
-        }))
-        .on("error", notify.onError({message: 'LESS compile error: <%= error.message %>'}))
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(notify('Compiled styles'));
+    return src( [ 'src/less/style.less' ], {base: "."} )
+    .pipe( concat('platform.less') )
+    //must write to public before processing for paths to work
+    // .pipe( dest('dist/css/') )
+    .pipe( less({ plugins: [autoprefix], paths: ['./src/less']}) )
+    .on( "error", notify.onError({message: 'LESS compile error: <%= error.message %>'}) )
+    .pipe( dest('dist/css/') )
+    .pipe( cssmin() )
+    .pipe( rename({ suffix: '.min' }) )
+    .pipe( dest('dist/css/') )
+    .pipe( notify('Compiled styles') );
+}
 
-
+function portalLessTask() {
     //Build style includes needed by WP Portal Theme
-    gulp.src([ 'src/less/portal/style.less' ], {base: "."})
-        .pipe(concat('portal.less'))
-        //must write to public before processing for paths to work
-        // .pipe(gulp.dest('dist/css/'))
-        .pipe(less({
-            plugins: [autoprefix],
-            paths: ['./src/less']
-        }))
-        .on("error", notify.onError({message: 'LESS compile error: <%= error.message %>'}))
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(notify('Compiled styles'));
-});
+    return src( [ 'src/less/portal/style.less' ], {base: "."} )
+    .pipe( concat('portal.less') )
+    //must write to public before processing for paths to work
+    // .pipe( dest('dist/css/') )
+    .pipe( less({ plugins: [autoprefix], paths: ['./src/less'] }) )
+    .on("error", notify.onError({message: 'LESS compile error: <%= error.message %>'}))
+    .pipe( dest('dist/css/') )
+    .pipe( cssmin() )
+    .pipe( rename({ suffix: '.min' }) )
+    .pipe( dest('dist/css/') )
+    .pipe( notify('Compiled styles') );
+}
 
-gulp.task('default', ['less', 'js']);
+
+exports.default = series(lessTask, portalLessTask, jshintTask, jsTask);
+exports.less = series(lessTask, portalLessTask);
+exports.jshint = jshintTask;
+exports.jsTask = series(jshintTask, jsTask);
